4a09a0a4eaefede97bceb5764ae91781b712a119
Antonio Daniswara <antonio.daniswara@gmail.com>
2019-11-23 15:50:25 +1100

    implement blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from .extensions import mongo,JSONEncoder
 
-def create_app(config_object='product_info_flask_restful.settings'):
+def create_app(config_object='app.settings'):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
     mongo.init_app(app)
     app.json_encoder = JSONEncoder
-    api = Api(app)
-    from .api.Products import ProductList
 
-    api.add_resource(ProductList, '/products')
+
+    from .api.Products import bp as product_bp
+    app.register_blueprint(product_bp)
     return app
--- app/api/Products/__init__.py
+++ app/api/Products/__init__.py
@@ -1,13 +1,18 @@
-from flask_restful import Resource, reqparse, fields, marshal
-from product_info_flask_restful.extensions import mongo
+from flask import Blueprint
+from flask_restful import Api, Resource, reqparse, fields, marshal
+from app.extensions import mongo
 from bson.objectid import ObjectId
 
 product_fields = {
+    '_id': fields.String,
     'title': fields.String,
     'category': fields.String,
     'price': fields.Float,
     'pic_url': fields.String
 }
+
+bp  = Blueprint('products', __name__)
+api = Api(bp)
 
 class ProductList(Resource):
     def get(self):
@@ -18,3 +23,18 @@ class ProductList(Resource):
         return  [marshal(task, product_fields) for task in resp]
     def post(self):
         pass
+
+
+class Product(Resource):
+    def get(self,id):
+        collection = mongo.db.products
+        resp = collection.find_one({ "_id": ObjectId(oid=str(id)) })
+        return marshal(resp, product_fields)
+    def patch(self,id):
+        pass
+    def delete(self,id):
+        pass
+
+
+api.add_resource(ProductList, '/products')
+api.add_resource(Product, '/products/<id>')
